461bab20319e8819c06096923d768f67c545b27e
MohdAnas07 <anas98144@gmail.com>
2023-02-08 15:31:53 +0530

    setup node for api create

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const appData = require('./appData.json');
+
+const port = process.env.PORT || 3000;
+
+// Middleware passed 
+app.use(cors());
+
+
+// routes ======================
+app.get("/", (req, res) => {
+    res.send("I am Live...")
+})
+
+
+app.get('/service', (req, res) => {
+    res.send(appData);
+})
+
+
+
+app.listen(port, () => {
+    console.log(`server is running at PORT ${port}`)
+})
+
+
+
