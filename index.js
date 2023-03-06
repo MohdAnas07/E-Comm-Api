8a4492798c50df6d94d5023b4d6f3c986cd9ec4b
MohdAnas07 <anas98144@gmail.com>
2023-03-06 17:30:03 +0530

    fake store api implementation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const appData = require('./appData.json');
+const axios = require('axios');
 
 const port = process.env.PORT || 3000;
 
@@ -13,16 +14,62 @@ app.get("/", (req, res) => {
     res.send("I am Live...")
 })
 
+app.get("/products", async (req, res) => {
+    const products = await axios('https://fakestoreapi.com/products');
+    const prodData = products.data
+    // Sort results
+    if (req.query.sort) {
+        prodData.sort((a, b) => {
+            return b.id - a.id
+        })
+    }
+    res.send(prodData)
+})
+
+// Get a single product
+app.get("/products/:id", async (req, res) => {
+    const ID = req.params.id
+    const product = await axios(`https://fakestoreapi.com/products/${ID}`);
+    res.send(product.data)
+})
+
+// Get all categories
+app.get("/products/categories", async (req, res) => {
+    const product = await axios(`https://fakestoreapi.com/products/categories`);
+    res.send(product.data)
+})
+
+// Get products in a specific category
+app.get("/products/categories/jewelery", async (req, res) => {
+    const product = await axios(`https://fakestoreapi.com/products/category/jewelery`);
+    res.send(product.data)
+})
+
+// Get products in a specific category
+
+app.post("/products", async (req, res) => {
+    const header = {
+        method: "POST",
+        body: JSON.stringify({
+            "title": "test product",
+            "price": 13.5,
+            "description": "lorem ipsum set",
+            "image": "https://i.pravatar.cc",
+            "category": "electronic"
+        })
+    }
+    const product = await axios(`https://fakestoreapi.com/products`, header);
+    res.send(product.data)
+})
+
+
+
+// Get service
 app.get('/service', (req, res) => {
     res.send(appData);
 })
 
-
-
-
 app.listen(port, () => {
     console.log(`server is running at PORT ${port}`)
 })
-
-
 
---
+++ productData.js
@@ -0,0 +1,9 @@
+// const axios = require('axios');
+
+// const getProducts = async () => {
+//     const URL = 'https://fakestoreapi.com/products';
+//     const res = await axios(URL);
+//     return res.data
+// }
+
+// module.exports = getProducts();
